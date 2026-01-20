2bdb76a355321c3ff79779b7b2b8e7468a02591a
Neeraj Narayanan <nfornj@gmail.com>
2026-01-19 19:20:20 -0500

    Force dark mode for sign-in page

--- frontend/src/auth-sign-in/SignInSide.tsx
+++ frontend/src/auth-sign-in/SignInSide.tsx
@@ -1,40 +1,30 @@
 import CssBaseline from '@mui/material/CssBaseline';
 import Stack from '@mui/material/Stack';
 import AppTheme from '../auth-shared-theme/AppTheme';
-import ColorModeSelect from '../auth-shared-theme/ColorModeSelect';
 import SignInCard from './components/SignInCard';
 import Content from './components/Content';
 
 export default function SignInSide(props: { disableCustomTheme?: boolean }) {
   return (
-    <AppTheme {...props}>
+    <AppTheme {...props} defaultMode="dark">
       <CssBaseline enableColorScheme />
-      <ColorModeSelect sx={{ position: 'fixed', top: '1rem', right: '1rem' }} />
       <Stack
         direction="column"
         component="main"
         sx={[
           {
             justifyContent: 'center',
-            height: 'calc((1 - var(--template-frame-height, 0)) * 100%)',
-            marginTop: 'max(40px - var(--template-frame-height, 0px), 0px)',
+            height: '100vh',
             minHeight: '100%',
           },
           (theme) => ({
-            '&::before': {
-              content: '""',
-              display: 'block',
-              position: 'absolute',
-              zIndex: -1,
-              inset: 0,
+            backgroundImage:
+              'radial-gradient(ellipse at 50% 50%, hsl(210, 100%, 16%), hsl(220, 30%, 5%))',
+            backgroundRepeat: 'no-repeat',
+            ...theme.applyStyles('light', {
               backgroundImage:
                 'radial-gradient(ellipse at 50% 50%, hsl(210, 100%, 97%), hsl(0, 0%, 100%))',
-              backgroundRepeat: 'no-repeat',
-              ...theme.applyStyles('dark', {
-                backgroundImage:
-                  'radial-gradient(at 50% 50%, hsla(210, 100%, 16%, 0.5), hsl(220, 30%, 5%))',
-              }),
-            },
+            }),
           }),
         ]}
       >
@@ -43,22 +33,13 @@ export default function SignInSide(props: { disableCustomTheme?: boolean }) {
           sx={{
             justifyContent: 'center',
             gap: { xs: 6, sm: 12 },
-            p: 2,
-            mx: 'auto',
+            p: { xs: 2, sm: 4 },
+            m: 'auto',
+            maxWidth: 1200,
           }}
         >
-          <Stack
-            direction={{ xs: 'column-reverse', md: 'row' }}
-            sx={{
-              justifyContent: 'center',
-              gap: { xs: 6, sm: 12 },
-              p: { xs: 2, sm: 4 },
-              m: 'auto',
-            }}
-          >
-            <Content />
-            <SignInCard />
-          </Stack>
+          <Content />
+          <SignInCard />
         </Stack>
       </Stack>
     </AppTheme>
